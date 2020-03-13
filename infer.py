8a6d6f5987e007f77d807485f2a7cfdb023d6015
davidlin <davidlindev@gmail.com>
2020-03-13 14:51:15 +0800

    ad

--- infer.py
+++ infer.py
@@ -7,14 +7,14 @@ import numpy
 import cv2
 
 import paddle
-from paddle import fluid 
-from util import * 
+from paddle import fluid
+from util import *
 import nibabel as nib
 from tqdm import tqdm
-import time 
+import time
 
 def parse_args():
-    parser = argparse.ArgumentParser("fit_a_line")
+    parser = argparse.ArgumentParser("liverseg")
     parser.add_argument(
                         '--use_gpu',
                         type=bool,
@@ -27,7 +27,7 @@ def main():
     use_cuda = args.use_gpu
     place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
     exe = fluid.Executor(place)
-    
+
     infer_exe = fluid.Executor(place)
     inference_scope = fluid.core.Scope()
 
@@ -35,10 +35,10 @@ def main():
     infer_param_path="/home/aistudio/work/params/unet_base_16_iou965/inf/"
     if not os.path.exists(inference_label_path):
         os.makedirs(inference_label_path)
-    
+
     with fluid.scope_guard(inference_scope):
         [inference_program, feed_target_names, fetch_targets] = fluid.io.load_inference_model(infer_param_path, infer_exe)
-        
+
         inf_volumes = os.listdir(inference_path)
         for inf_volume in tqdm(inf_volumes, position=0):
             total_time = 0
@@ -48,17 +48,17 @@ def main():
             post_time = 0
 
             total_time = time.time()
-            
+
             inf_path = os.path.join(inference_path, inf_volume)
             # print(inf_path)
-            
+
             read_time = time.time()
             volf = nib.load(inf_path)
             volume = np.array(volf.get_fdata())
             volume = volume.clip(-1024, 1024)
             inference = np.zeros(volume.shape)
             read_time = time.time() - read_time
-            
+
             batch_size = 8
             ind = 0
             flag = True
@@ -86,7 +86,7 @@ def main():
                 result = infer_exe.run(inference_program, feed={feed_target_names[0]: batch_data }, fetch_list=fetch_targets)
                 toc = time.time()
                 inf_time += toc - tic
-                
+
                 tic = time.time()
                 result = np.array(result)
                 result = result.reshape([-1, 2, 512, 512])
@@ -101,7 +101,7 @@ def main():
                     ii = ii - 1
                 # result = result * 255
                 # cv2.imwrite("result.png",result)
-                
+
                 toc = time.time()
                 post_time += toc - tic
 
@@ -111,9 +111,9 @@ def main():
             inference_file = nib.Nifti1Image(inference, volf.affine)
             nib.save(inference_file, os.path.join(inference_label_path, inf_volume))
             write_time = time.time() - write_time
-            
+
             total_time = time.time() - total_time
-            
+
 
             # print("Total process time: " + str(total_time) )
             # print("\t read file time: " + str(read_time))
@@ -121,12 +121,10 @@ def main():
             # print("\t post process time: " + str(post_time))
             # print("\t write file time: " + str(write_time))
             # print("\n\n\n")
-            
-            
+
+
             pbar.close()
-            
+
 if __name__ == '__main__':
     args = parse_args()
     main()
-
-
