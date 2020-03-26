710fa111516922dde621a55684c7f0b66097c66b
davidlin <davidlindev@gmail.com>
2020-03-26 13:02:32 +0800

    add

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # liverSeg
-基于paddle框架的肝脏及肝脏肿瘤分割。这个项目目前还是work in progress，但是在肝脏上已经做到.92的准确率. 目前支持2.5D Unet,Res-Unet和DeepLab. 后期会加入更多网络结构和loss做成一个小框架。
-
+基于paddle框架的肝脏及肝脏肿瘤分割。这个项目目前还在修改，但是在肝脏上已经做到.92的准确率，后期会加入更多网络结构和loss做成一个小框架。
 ## 项目结构
 - config.py 包含项目配置，主要是文件路径
-- preprocess.py 进行数据预处理，主要将3D体数据保存为2.5D npy
+- preprocess.py 进行数据预处理，将3D体数据保存为2.5Dnpy
 - train.py 进行Unet训练
 - infer.py 进行前向推理
 
@@ -28,7 +27,7 @@ python preprocess.py
 python train.py --use_gpu = True --num_epochs = 20
 ```
 ### 预测
-最后一步是用训练好的网络进行预测，模型权重的路径在 infer.py 中注意修改，按照上一步实际输出的路径进行修改。代码会读取inference路径下所有的nii逐个进行预测。目前支持的数据格式有 .nii, .nii.gz。预训练权重在[这个](https://github.com/davidlinhl/weights)项目中可以下载。
+最后一步是用训练好的网络进行预测，模型权重的路径在代码中，按照上一步实际输出的路径进行修改。代码会读取inference路径下所有的nii逐个进行预测。目前支持的数据格式有 .nii, .nii.gz。
 ```shell
 python infer.py -use_gpu = True
 ```
--- z_prep.py
+++ z_prep.py
@@ -26,26 +26,26 @@ for i in range(len(labels)):
 	pbar.set_postfix(filename=labels[i].rstrip(".nii"))
 	pbar.update(1)
 
-# 	print(volumes[i], labels[i])
+	print(volumes[i], labels[i])
 
 	volf = nib.load(os.path.join(volumes_path, volumes[i]))
 	labf = nib.load(os.path.join(labels_path, labels[i]))
 
 	header = volf.header.structarr
 	save_info(volumes[i], header, 'vol_info.csv')
-	
+
 	volume = volf.get_fdata()
 	label = labf.get_fdata()
 
 # 	volume=np.clip(volume,-1024,1024)
 	volume = windowlize_image(volume, 500, 30)
 	label = clip_label(label, 1)
-	
+
 	spacing = [1, 1, 1]
 	pixdim = [header['pixdim'][1], header['pixdim'][2], header['pixdim'][3]]  # pixdim 是这张 ct 三个维度的间距
 	ratio = [pixdim[0]/spacing[0], pixdim[1]/spacing[1], pixdim[2]/spacing[2]]
 	ratio = [1, 1, ratio[2]]
-	
+
 	volume=scipy.ndimage.interpolation.zoom(volume,ratio,order=3)
 	label=scipy.ndimage.interpolation.zoom(label,ratio,order=0)
 
@@ -67,25 +67,22 @@ for i in range(len(labels)):
 	print(label.shape)
 	volume = volume.astype(np.float16)
 	label = label.astype(np.int8)
+	# volume = np.ones( [512, 512, 615])
+	# label = np.ones([512, 512, 615])
 
 	for frame in range(1,volume.shape[0]-1):
 		if np.sum(label[frame ,: ,:]) > 32:
 			vol=volume[frame-1: frame+2, :, :]
 			lab=label[frame, :, :]
-			lab = lab.reshape([lab.shape[0], lab.shape[1], 1])
+			lab = lab.reshape([1, lab.shape[0], lab.shape[1]])
 
-
-			# vol=np.swapaxes(vol,0,2)
-# 			lab=np.swapaxes(lab,0,2)  #[3,512,512],3 2 1 的顺序，用的时候倒回来, CWH
-			lab = lab.reshape([1, 512, 512])
-			
-			vol = volume[:, :, 0:512]
+			vol = vol[:, :, 0:512]
 			lab = lab[:, :, 0:512]
 
-			# print(vol.shape)
-			# print(lab.shape)
+			print(vol.shape)
+			print(lab.shape)
 
-			data=np.concatenate((vol,lab),axis=0)
+			data=np.concatenate((vol, lab), axis=0)
 			file_name = "lits{}-{}.npy".format(volumes[i].rstrip(".nii").lstrip("volume"),frame)
 			np.save(os.path.join(preprocess_path, file_name),data )
 
